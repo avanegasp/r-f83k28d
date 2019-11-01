2bbd0de3d98965cb10d3a15690245180a0a630d3
Angie <arco527@gmail.com>
2019-11-01 14:36:41 -0500

    Challenge valid

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
+// import React, { Fragment, useState } from 'react';
+//
+// export const App = () => {
+//
+//   const [count, setCount] = useState(0)
+//
+//   return (
+//     <Fragment>
+//       <span className="value">{count}</span>
+//       <button
+//         id="inc"
+//         onClick={ () => setCount(count + 1) }
+//       >
+//         Incrementa
+//       </button>
+//     </Fragment>
+//   );
+// };
+
 import React, { Component } from 'react';
-import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clicks: 0,
+      show: true
+    };
+  }
+
+  IncrementItem = () => {
+    this.setState({ clicks: this.state.clicks + 1 });
+  }
   render() {
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+      <span className="value">{this.state.clicks }</span>
+        <button id="inc" onClick={this.IncrementItem}>Incrementa</button>
+        { this.state.show ? <h2>{ this.state.clicks }</h2> : '' }
       </div>
     );
   }
